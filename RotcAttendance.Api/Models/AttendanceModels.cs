dfd2e91891e4ec8e86e2dec0b459c1ed639c1cff
Michael Angelo <Michael Angelo@DESKTOP-DSMC0PG>
2026-07-30 00:30:54 +0800

    Remove duplicate backend project

--- RotcAttendance.Api/Models/AttendanceModels.cs
+++
@@ -1,6 +0,0 @@
-namespace RotcAttendance.Api.Models;
-
-// QR attendance support is implemented through the existing User, CadetProfile, and AttendanceRecord models.
-public static class AttendanceModelNotes
-{
-}
